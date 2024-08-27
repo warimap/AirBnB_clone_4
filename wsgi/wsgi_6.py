4253c7025fa105068a7de6d1733078c657647b7e
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-27 12:01:59 +0300

    Update wsgi_6.py

--- wsgi/wsgi_6.py
+++ wsgi/wsgi_6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 imports Flask instance for gunicorn configurations
-gunicorn --bind 127.0.0.1:8001 wsgi:web_flask.app
+gunicorn --bind 34.204.60.71:8001 wsgi:web_flask.app
 """
 
 web_flask = __import__('web_flask.6-number_odd_or_even',
