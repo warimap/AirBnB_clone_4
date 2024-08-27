a402ef2fe5a16affe0205ea5210c79660e49822b
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-27 12:02:19 +0300

    Update wsgi_api.py

--- wsgi/wsgi_api.py
+++ wsgi/wsgi_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 imports API App for gunicorn configurations
-gunicorn --bind 127.0.0.1:8004 wsgi.wsgi_api:app
+gunicorn --bind 34.204.60.71:8004 wsgi.wsgi_api:app
 """
 app = __import__('api.v1.app', globals(), locals(), ['*'])
 
