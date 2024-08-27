7d5d05bcd048ba472a90b1d3c0ff93f776690bc7
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-27 12:01:31 +0300

    Update wsgi.py

--- wsgi/wsgi.py
+++ wsgi/wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 imports Flask instance for gunicorn configurations
-gunicorn --bind 127.0.0.1:8001 wsgi:web_flask.app
+gunicorn --bind 34.204.60.71:8001 wsgi:web_flask.app
 """
 
 web_flask = __import__('web_flask.0-hello_route', globals(), locals(), ['*'])
